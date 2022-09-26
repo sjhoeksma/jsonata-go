0518b7ab7a529ce8d36558268d75c47f31ce4c53
Sierk Hoeksma <sierk@hoeksma.org>
2022-09-26 11:02:40 +0200

    License generalization

--- jparse/lexer.go
+++ jparse/lexer.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Blues Inc.  All rights reserved.
+// All rights reserved.
 // Use of this source code is governed by licenses granted by the
 // copyright holder including that found in the LICENSE file.
 
